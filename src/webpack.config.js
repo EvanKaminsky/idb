43fee914b60d217f69f3e93c11f57750ea168cdb
Evan Kaminsky <evanskaminsky@gmail.com>
2017-10-18 23:00:37 -0500

    react overhaul

---
+++ src/webpack.config.js
@@ -0,0 +1,19 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: ["./frontend/js/app.js"],
+    output: {
+        path: __dirname + '/frontend/static',
+        filename: "bundle.js"
+    },
+
+    module: {
+        loaders: [{
+            test: /\.js?$/,
+            loader: 'babel-loader',
+            query: {presets: ['es2015', 'react']},
+            exclude: /node_modules/
+        }]
+    },
+    plugins: []
+};
