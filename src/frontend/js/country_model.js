cd4d2050aa6b334c6d3b331dd452041d214eb7aa
shinobhi <cilindra@gmail.com>
2017-10-21 12:17:12 -0500

    edited models for cocktails and countries

---
+++ src/frontend/js/country_model.js
@@ -0,0 +1,14 @@
+class cocktail_model extends React.Component {
+  render() {
+    return (
+      <div class = "col-md-6 cocktail-box">
+      <img class="img-responsive" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Flag_of_Mexico.svg/840px-Flag_of_Mexico.svg.png"/>
+      <h2>{this.countries.name}</h2>
+      <h3>Ingredients</h3>
+      <p><a href="../ingredients/tequila.html">Tequila</a>, <a href="../ingredients/kahlua.html">Kahlua</a></p>
+      <h3>Brands</h3>
+      <p><a href="../brands/olmeca.html">Olmeca</a></p>
+      </div>
+    );
+  }
+}
