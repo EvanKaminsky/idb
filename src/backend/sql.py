0789ae2900ded12d2d83524b1b4e1e6bab247b0a
bkustar <bkustar@gmail.com>
2017-10-25 02:58:14 -0500

    database improvements

--- src/backend/sql.py
+++ src/backend/sql.py
@@ -1,10 +1,23 @@
 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import create_engine
+
 # Low-level python wrapper around our database
+
+engine = create_engine('mysql+mysqldb://root:tipsymix@/tipsy_backend?unix_socket=/cloudsql/tipsymix-ttp:tipsy-db')
+
+# for local testing use:
+# engine = create_engine('mysql+mysqldb://root:tipsymix@localhost:3306/tipsy_backend)
+db = engine.connect()
 
 # SELECT query
 # 	+ SELECT select_q FROM from_q WHERE where_q
 def sql_select(select_q, from_q, where_q):
-	# implement this
+    qString = "SELECT "+str(select_q)+" FROM "+str(from_q)
+    if(where_q not None):
+        qString += " WHERE "+str(where_q)
+	result = db.execute(qString)
 	return {}
 
 # Fetch all instances of a class
