7a0db24d56a6b3fad4db01b284950923f9cc42d7
Abhi Ilindra <cilindra@gmail.com>
2017-10-26 00:15:06 -0500

    Cocktails page works with static content, waiting on Evan so I can write code to populate with dynamic content

--- src/frontend/js/Pages/CocktailsPage.js
+++ src/frontend/js/Pages/CocktailsPage.js
@@ -2,12 +2,53 @@ import React from 'react';
 
 import Page from "./Page.js";
 
+import "../../static/css/about.css"
+let backgroundURL = "/static/public/index.jpg";
+
+const background = {
+    backgroundImage: 'url(' + backgroundURL + ')',
+    flex: 1,
+    resizeMode: 'cover', // or 'stretch'
+};
+
 export default class CocktailsPage extends React.Component {
     render() {
         return (
-            <div>
-                <Page title="Cocktails"/>
-            </div>
+            <body style = {background}>
+                <h1>Tipsy Mix</h1>
+
+                <div id = "searchForm">
+                  <input type="text" class="search" placeholder="Search by ingredients, cocktail, country, or brand" /><br/>
+                  <input type="submit" class="searchButton" placeholder="Search" />
+                </div>
+
+
+                <section class = "container">
+                  <div class = "row">
+                    <div class = "col-md-3 cocktail-box">
+                      <img class= "img-responsive" src="http://www.1001cocktails.com/images/like-hand-grenade-5504.jpg" />
+                      <h5>Like Hand Grenade</h5>
+                      <p>gin, grain alcohol, melon liquer, rum, vodka</p>
+                      <a href="cocktails/likeHandGrenade.html" class="btn btn-info btn-log" role ="button">More</a>
+                    </div>
+
+                    <div class = "col-md-3 col-md-offset-1 cocktail-box">
+                      <img class="img-responsive" src="http://www.1001cocktails.com/images/flaming-lamborghini-5541.jpg" />
+                      <h5>Flaming Lamborghini</h5>
+                      <p>coffe liqueuer (tia maria, kahlua), sambuca, blue curacao liqueur, irish cream (bailey's)</p>
+                      <a href="cocktails/flamingLamborghini.html" class="btn btn-info" role ="button">More</a>
+                    </div>
+
+                    <div class = "col-md-3 col-md-offset-1 cocktail-box">
+                      <img class="img-responsive" src="http://www.1001cocktails.com/images/blue-lagoon-50.jpg"/>
+                      <h5>Blue Lagoon</h5>
+                      <p>vodka, blue curacao liquer, lemonade, cherry</p>
+                      <a href="cocktails/blueLagoon.html" class="btn btn-info" role ="button">More</a>
+                    </div>
+
+                  </div>
+                </section>
+              </body>
         )
     }
 }
--- src/frontend/js/country_model.js
+++ src/frontend/js/country_model.js
@@ -3,7 +3,7 @@ class country_model extends React.Component {
     return (
       <div class = "col-md-6 cocktail-box">
       <img class="img-responsive" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Flag_of_Mexico.svg/840px-Flag_of_Mexico.svg.png"/>
-      <h2>{this.countries.name}</h2>
+      <h2>{this.props.name}</h2>
       <h3>Ingredients</h3>
       <p><a href="../ingredients/tequila.html">Tequila</a>, <a href="../ingredients/kahlua.html">Kahlua</a></p>
       <h3>Brands</h3>
