92d37e7e70655f5f21b0dea87d63281152c0ef4e
Evan Kaminsky <evanskaminsky@gmail.com>
2017-10-27 00:03:19 -0500

    this cors thing is ridiculous

--- src/main.py
+++ src/main.py
@@ -85,11 +85,11 @@ def react(path):
 
 @app.after_request
 def after_request(response):
-    print("CORS")
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
     return response
+
 
 ################################
 # Running Flask Locally #
