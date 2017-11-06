a6d3403385559f49f79982e56f193efd897b48f3
Abhi Ilindra <cilindra@gmail.com>
2017-11-05 18:53:02 -0600

    fixed merge conflicts for real this time

--- src/frontend/js/app.js
+++ src/frontend/js/app.js
@@ -11,8 +11,6 @@ import IngredientsPage  from './pages/IngredientsPage.js';
 import BrandsPage       from './pages/BrandsPage.js';
 import CountriesPage    from './pages/CountriesPage.js';
 import AboutPage        from './pages/AboutPage.js';
-import CocktailModel    from './details/cocktail_model.js'
-import BrandModel       from './details/brand_model.js'
 
 import CocktailDetailPage   from "./details/cocktail_model.js";
 import IngredientDetailPage from "./details/ingredient_model.js";
@@ -50,42 +48,10 @@ class TipsyApp extends React.Component {
                     <Route path='/country-detail/:slug'     component={CountryDetailPage}/>
                 </Switch>
 
-<<<<<<< HEAD
-const brands = require('json-loader!../spoof/testdata_brand.json');
-const cocktails = require('json-loader!../spoof/testdata_cocktail.json');
-const countries = require('json-loader!../spoof/testdata_country.json');
-
-ReactDOM.render((
-    <BrowserRouter>
-        <div>
-=======
             </div>
         </BrowserRouter>
     )}
->>>>>>> b365926e94754beed2ca810bd1935e0ff7d52414
 
 }
 
-<<<<<<< HEAD
-            <Switch>
-                <Route exact path='/'       component={HomePage}/>
-                <Route path='/cocktails'    component={CocktailsPage}/>
-                <Route path='/ingredients'  component={IngredientsPage}/>
-                <Route path='/brands'       component={BrandsPage}/>
-                <Route path='/about'        component={AboutPage}/>
-                <Route path='/countries'    component={CountriesPage}/>
-                {brands.map(function(item, i) {
-                    return(<Route path={'/' + item.stdname} component={BrandModel}/>);
-                })}
-                {cocktails.map(function(item, i) {
-                    return(<Route path={'/' + item.stdname} component={CocktailModel}/>);
-                })}
-                {countries.map(function(item, i) {
-                    return(<Route path={'/' + item.stdname} component={AboutPage}/>);
-                })}
-            </Switch>
-=======
 ReactDOM.render(<TipsyApp/>, document.getElementById('reactEntry'));
->>>>>>> b365926e94754beed2ca810bd1935e0ff7d52414
-
-
