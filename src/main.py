e500e6d00f58b4edd160bfb8282eaa561eb92626
micaKohl <mica.kohl@utexas.edu>
2017-11-09 21:35:16 -0600

    unquote the filterRules and the query

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ TipsyMix's Flask Backend Interface
 
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS, cross_origin
+from urllib import unquote
 
 from backend import api
 
@@ -28,8 +29,8 @@ base_api = "/api/"
 @cross_origin()
 def search():
     category =      request.args.get('category', default=None, type=str)
-    query =         request.args.get('query', default=None, type=str)
-    filter_rules =  request.args.get('filterRules', default=None, type=str)
+    query =         request.args.get(unquote('query'), default=None, type=str)
+    filter_rules =  request.args.get(unquote('filterRules'), default=None, type=str)
     count =         request.args.get('count', default=None, type=int)
     page =          request.args.get('page', default=None, type=int)
     pagesize =      request.args.get('pagesize', default=None, type=int)
