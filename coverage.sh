9c09f09ae9561d39859c51d08b9ba1eeee068817
bkustar <bkustar@gmail.com>
2017-11-14 12:12:11 -0600

    made coverage work on macs

--- coverage.sh
+++ coverage.sh
@@ -5,8 +5,7 @@ cp -rf tests/*.py tests/coverage
 cp -rf backend/* tests/coverage
 cd tests/coverage
 sed -i -e 's/from backend //g' *.py
-coverage2 run --omit=/usr/* --include=./*.py runTests.py
-coverage2 html
-coverage2 report -m
-open ./htmlcov/index.html
-xdg-open ./htmlcov/index.html
+coverage2 run --omit=/usr/* --include=./*.py runTests.py || coverage run --omit=/usr/* --include=./*.py runTests.py
+coverage2 html || coverage html
+coverage2 report -m || coverage report -m
+open ./htmlcov/index.html || xdg-open ./htmlcov/index.html
